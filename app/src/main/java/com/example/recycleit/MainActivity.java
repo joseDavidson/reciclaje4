9d02211191832a240f1ca68a41c5fc8c044f41f7
José Davidson <nacho.davidson@gmail.com>
2021-06-25 17:18:05 -0400

    comit prueba comentario 33333

--- app/src/main/java/com/example/recycleit/MainActivity.java
+++ app/src/main/java/com/example/recycleit/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             btnRegistrarse,btnEntrarInvitado,btnDenunciarPunto
             ,btnRegistrarUsuario;
     private ImageButton btnPorPunto,btnPorRecolector;
-    //comentario 2 qlo sdfgsfsdfsfd
+    //comentario34433
 
 
 
